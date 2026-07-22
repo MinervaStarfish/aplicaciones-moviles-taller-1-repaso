9f10d4ccae871e74453626d016f422330f1c8a50
Mariana <osoriorojasmariana@gmail.com>
2026-07-22 10:01:11 -0500

    Reto 1: clave banco

---
+++ clave/script.js
@@ -0,0 +1,39 @@
+let password = "";
+
+function agregar(numero) {
+
+    if (password.length < 4) {
+
+        password += numero;
+
+        document.getElementById("clave").value = password;
+
+    }
+
+}
+
+function borrar() {
+
+    password = password.slice(0, -1);
+
+    document.getElementById("clave").value = password;
+
+}
+
+function validar() {
+
+    if (password === "1234") {
+
+        alert("Bienvenido");
+
+    } else {
+
+        alert("Clave incorrecta");
+
+    }
+
+    password = "";
+
+    document.getElementById("clave").value = "";
+
+}
