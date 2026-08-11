ed3d22bae5ae48cfb5fc24e3f1da1b0bdfb71176
Mariana <osoriorojasmariana@gmail.com>
2026-08-10 19:08:54 -0500

    reto menu update

--- menu/script.js
+++ menu/script.js
@@ -1,19 +1,11 @@
 const links = document.querySelectorAll("nav a");
 
 links.forEach(link => {
-
     link.addEventListener("click", function (e) {
-
         e.preventDefault();
-
         const destino = document.querySelector(this.getAttribute("href"));
-
         destino.scrollIntoView({
-
             behavior: "smooth"
-
         });
-
     });
-
 });
